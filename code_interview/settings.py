612168ee7346fe516040e633926371dac341b6a5
Abdullah Mirza <86431838+Mirzeeyass@users.noreply.github.com>
2022-08-17 05:09:32 -0700

    Update settings.py

--- code_interview/settings.py
+++ code_interview/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'j%4s9n79np!^nrq3&h4=6a8r2c^ex9s)gg3s(zsx((o@qll2yj'
 DEBUG = True
-ALLOWED_HOSTS = ['abdcoderview.herokuapp.com','localhost','coderview.co']
+ALLOWED_HOSTS = ['abdcoderview.herokuapp.com','localhost','http://coderview.co']
 
 # Celery config
 
@@ -99,7 +99,7 @@ CHANNEL_LAYERS = {
 }
 
 CORS_ORIGIN_WHITELIST = [
-    'http://coderview.co/'
+    'http://coderview.co'
     # front-end
 ]
 
